3d72c815b9fcdbc9aed8553effa0e68897529111
Flacko <lesliejoe2k2@gmail.com>
2024-08-07 20:53:52 +0000

    created cart slice

--- src/store/slices/cart-slice.js
+++ src/store/slices/cart-slice.js
@@ -1,2 +1,7 @@
 // using redux toolkit for state managenent
-//
+// createSlice (import)
+// initialize initial state 
+// slice(create) - mentio intial state, actions
+
+import { createSlice } from "@reduxjs/toolkit";
+
